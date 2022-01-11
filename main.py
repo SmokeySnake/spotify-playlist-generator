20398fe84dae28076a28b9316b08c8184de97f07
SmokeySnake <alice.knox96@gmail.com>
2022-01-11 16:25:41 +0000

    Adds main and requirements
    - gitignore
      -modified file
    
    - main
      - created main
      - imports spotipy
      - prints hello world
    
    - requirements
      - spotipy is required

---
+++ main.py
@@ -0,0 +1,20 @@
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+#Take a list of song names in a text file 
+#and use this to cxreate a Spotify playlist
+
+def main():
+    print("hello world")
+
+
+#create a list to add song ids
+#take each line in the test file of song names
+#search for each song name 
+#get the song id
+#add this to the list created above
+#when all song ids have been added 
+
+if __name__ == '__main__':
+    main()
