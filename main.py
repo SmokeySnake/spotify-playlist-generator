93cfbc84ffd46332a40118634e09f1cb7ac30eb3
SmokeySnake <alice.knox96@gmail.com>
2022-01-12 18:55:10 +0000

    Add Spotify LOgin and Authentication
    -main
      - Adds spotify user login
      - adds the creation of an authenticated spotify image for use with playlist creation

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 
-from os import listdir
+from os import listdir, read
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from json import load as jload
 
 # Take a list of song names in a text file
 # and use this to cxreate a Spotify playlist
@@ -14,7 +15,7 @@ def create_playlist(playlist_name: str, id_list: list) -> None:
     """
 
 
-def get_song_ids(songnames: list) -> list:
+def get_song_ids(spot: spotipy.Spotify, songnames: list) -> list:
     """
     search for each song name
     get the song id
@@ -56,14 +57,27 @@ def choose_playlist_name() -> str:
     print(f"\nYour playlist title is: {playlist_title}")
 
 
-def login():
-    print("login")
+def login() -> spotipy.Spotify:
+    with open("./config.json", mode="r") as user_cred_file:
+        user_cred = jload(user_cred_file)
+
+    client_id: str = user_cred["SPOTIPY_CLIENT_ID"]
+    client_secret: str = user_cred["SPOTIPY_CLIENT_SECRET"]
+    redirect_uri: str = user_cred["SPOTIPY_REDIRECT_URI"]
+
+    scope = "user-library-read"
+
+    spot_oa = SpotifyOAuth(scope=scope, client_id=client_id,
+                           client_secret=client_secret, redirect_uri=redirect_uri)
+    sp = spotipy.Spotify(auth_manager=spot_oa)
+
+    return sp
 
 
-def main():
+def main() -> None:
     print("\n Welcome to Spotify Playlist Generator! ", end="\n \n")
 
-    login()
+    spot: spotipy.Spotify = login()
 
     playlist_name: str = choose_playlist_name()
 
@@ -71,10 +85,10 @@ def main():
 
     song_names: list = read_playlist(file_name)
 
-    song_ids: list = get_song_ids(song_names)
+    song_ids: list = get_song_ids(spot, song_names)
 
-    create_playlist(playlist_name, song_ids)
+    create_playlist(spot, playlist_name, song_ids)
 
 
 if __name__ == '__main__':
-    main()
+    main() 
